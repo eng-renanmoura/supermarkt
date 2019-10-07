d8dab36506fab2fddfd97a66abd71536e49109fd
Renan Moura <eng.renanmoura@gmail.com>
2019-10-07 15:54:05 -0300

    Configuração de Segurança Spring security

---
+++ supermarkt-api/src/main/java/br/com/supermarkt/ConfiguracaoSeguranca.java
@@ -0,0 +1,54 @@
+package br.com.supermarkt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.com.supermarkt.seguranca.FiltroAutenticacaoJwt;
+import br.com.supermarkt.seguranca.JwtAuthenticationEntryPoint;
+import br.com.supermarkt.seguranca.UsuarioServico;
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
+
+	private UsuarioServico usuarioServico;
+	private FiltroAutenticacaoJwt filtroAutenticacaoJwt;
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/autenticacao/**").permitAll()
+			.anyRequest().authenticated()
+			.and().cors()
+			.and().csrf().disable()
+			.formLogin().disable()
+			.httpBasic().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.addFilterBefore(filtroAutenticacaoJwt, UsernamePasswordAuthenticationFilter.class)
+			.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
+	}
+	
+	@Override
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(usuarioServico).passwordEncoder(bCryptPasswordEncoder);
+	}
+
+	@Override
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+}
