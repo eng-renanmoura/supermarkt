07ac21a45b18aeeec01e7cd8b1720d9b522f783b
Renan Moura <eng.renanmoura@gmail.com>
2019-10-08 11:28:01 -0300

    modelo e api's do estoque e horário de funcionamento

---
+++ supermarkt-api/src/main/java/supermercado/EstoqueDto.java
@@ -0,0 +1,16 @@
+package supermercado;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class EstoqueDto {
+	
+	private Long id;
+	
+	public EstoqueDto(Estoque estoque) {
+		this(estoque.getId());
+	}
+
+}
