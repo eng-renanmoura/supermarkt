37897ce46ba597404dc7db6eb49b5603dc2d7385
Renan Moura <eng.renanmoura@gmail.com>
2019-11-17 14:42:04 -0300

    ajuste

--- supermarkt-ui/src/app/admin/admin.module.ts
+++ supermarkt-ui/src/app/admin/admin.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { AdminRoutingModule } from './admin-routing.module';
 
@@ -10,7 +9,6 @@ import { AdminRoutingModule } from './admin-routing.module';
   ],
   imports: [
     CommonModule,
-    FormsModule,
     AdminRoutingModule,
     SharedModule
    ]
